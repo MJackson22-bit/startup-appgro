4410e28500bd1e896598dce371da7531c9476c95
MJackson22-bit <jm5444859@gmail.com>
2022-07-06 01:49:16 -0600

    Add activity for show the games of animal

---
+++ app/src/main/java/com/example/startupappgro/animal/AnimalGamesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.startupappgro.animal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.startupappgro.R
+
+class AnimalGamesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animal_games)
+    }
+}
